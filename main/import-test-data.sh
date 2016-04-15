5c63da8f97da27ae2345d7233df0050180974c7a
Renat <rzhilkibaev@gmail.com>
2016-04-14 17:18:10 -0700

    Add 2dsphere indices for geoNear command

--- main/import-test-data.sh
+++ main/import-test-data.sh
@@ -16,7 +16,10 @@ done
 
 # now that we know db is up we can import the rest
 mongoimport /restaurants.json --db=test --collection=restaurants;
+mongo test --eval "db.restaurants.createIndex({ location: '2dsphere' })"
+
 mongoimport /neighborhoods.json --db=test --collection=neighborhoods;
+mongo test --eval "db.neighborhoods.createIndex({ geometry: '2dsphere' })"
 
 mongo admin --eval "db.shutdownServer({timeoutSecs: 3});"
 sleep 3
